19368245154e8655d42a209197f312b895e1ab2d
alexkm13 <95406259+alexkm13@users.noreply.github.com>
2023-09-13 23:21:32 -0400

    Create assign0-1.ml

---
+++ assigns/assign0/MySolution/assign0-1.ml
@@ -0,0 +1,12 @@
+*
+Assign0-1: 10 points
+Please find the first integer N such that the
+evaluation of fact(N) in OCaml yields an Overflow
+exception.
+*)
+
+let rec myloop(x: int): int =
+  if fact(x) = 0 then x else myloop(x+1)
+
+;;
+(* ****** ****** *)
